369688cf4049e569b654364cec3eb65cc4667d36
jon-stults <jon.stults@kalderos.com>
2021-03-10 00:26:18 -0600

    Update settings.py

--- pickMyFlick/settings.py
+++ pickMyFlick/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -90,6 +91,9 @@ DATABASES = {
         "PASSWORD": "Pugoogoo11!",
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
